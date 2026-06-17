26295ada3489bfdd0446e51833f763648c847fbb
Pankaj Chavan <156188537+pankajc2005@users.noreply.github.com>
2026-06-18 01:39:00 +0530

    fix(ci): commit bun.lock and add install step to security-scan job

--- test-reply.js
+++ test-reply.js
@@ -1,7 +1,8 @@
 const replies = JSON.stringify([
-    {
-        comment_id: "4705275722",
-        reply: "Acknowledged. I'll make sure all my work is verified. There were no further comments to act on."
-    }
+  {
+    comment_id: "4705275722",
+    reply:
+      "Acknowledged. I'll make sure all my work is verified. There were no further comments to act on.",
+  },
 ]);
 console.log(replies);
